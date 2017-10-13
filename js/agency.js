d74f5d0091a0b664876b6417c6c6dfdfe156fb10
Christopher Powell <powell.christopher@gmail.com>
2017-10-13 22:07:39 +0200

    improvements to GA

--- js/agency.js
+++ js/agency.js
@@ -5,8 +5,8 @@
  */
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
+$(function () {
+    $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
@@ -21,17 +21,37 @@ $('body').scrollspy({
 })
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
+$('.navbar-collapse ul li a').click(function () {
     $('.navbar-toggle:visible').click();
 });
 
-$('div.modal').on('show.bs.modal', function() {
-	var modal = this;
-	var hash = modal.id;
-	window.location.hash = hash;
-	window.onhashchange = function() {
-		if (!location.hash){
-			$(modal).modal('hide');
-		}
-	}
+$('div.modal').on('show.bs.modal', function () {
+    var modal = this;
+    var hash = modal.id;
+    window.location.hash = hash;
+    window.onhashchange = function () {
+        if (!location.hash) {
+            $(modal).modal('hide');
+        }
+    }
+
+    // send event to google analytics
+    var productType = modal.classList[0].split('-')[0];
+    var productName = $(modal).find('.modal-body').children('h2').text();
+    ga('send', {
+        hitType: 'event',
+        eventCategory: productType,
+        eventAction: 'view',
+        eventLabel: productName
+    });
+});
+
+$("a.page-scroll").click(function() {
+    // send event to google analytics
+    ga('send', {
+        hitType: 'event',
+        eventCategory: 'Navigation',
+        eventAction: 'view',
+        eventLabel: $(this).attr('href')
+    });
 });
